8203550d8196e2a95b89ccf5829def534c8636c8
HasancanCakicioglu <hckecommerce@gmail.com>
2024-07-26 18:35:05 +0300

    debug

--- src/actions/main.ts
+++ src/actions/main.ts
@@ -1,7 +1,6 @@
 "use server";
 import apiClient from "@lib/axios";
 import ErrorResponse from "@/entities/api_error";
-import { cookies } from "next/headers";
 
 import { handleApiError } from "./api";
 import { homeSuccessResponse } from "@/entities/home";
@@ -10,7 +9,6 @@ export async function home(): Promise<
 homeSuccessResponse | ErrorResponse
 > {
   try {
-    const token = cookies().get("access_token");
 
     const response = await apiClient.get<homeSuccessResponse>(
       "/main/home",
